a266b5b687bcd431712699b621170fd8af8e064c
Antonio Pierro <antonio.pierro@gmail.com>
2021-01-09 13:38:02 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,30 @@
+exports.blockExplorer = (address) =>
+    `https://etherscan.io/address/${address}#code`;
+
+// Ponzi scheme smart constract addresses from 2018-10-10 to 2020
+exports.addresses = [
+    "0x3df616ee25ba3fa146544ab595f106bff5ac4397",
+    "0x311f71389e3de68f7b2097ad02c6ad7b2dde4c71",
+    "0xf91446ead6aadd4321602b5790a6db4d6f2a1930",
+    "0xc249fe0288e75e47e62b138de8d020c9fee5a9f5",
+    "0x67ad530ecdfd7ac8289362914e88d018e8b3f3f0",
+    "0x2cdb253c0e44a284f6174ae90b5ea247e6cf3649",
+    "0xc601f6938c75c4ca03eaad570701d86e607771db",
+    "0xcd3dc4e19418fe7dad216376ff685760685cee13",
+    "0xfad05ee13c8e0ec94e5ee9dc056dc451b2ad6b1d",
+    "0x9b63e5599f8d7b70f96da0700bde1b4c0d7f35a9",
+    "0x7b307c1f0039f5d38770e15f8043b3dd26da5e8f",
+    "0xa25560d083fe0ea3e303c11577b5a345b236fac7",
+    "0x339150b0851bc5b3c0323789a58cc4e4afb38b37",
+    "0x02c60d28be3338014fef3fdf50a3218b946c0609",
+    "0xbf91b27fec3146a1b36069f92337a0ea330725df",
+    "0x0135c9a7bff72aa26e1d105ff5000e454e4dde7a",
+    "0x82aa987a16deafd8b4fab73ec316f94f4df43020",
+    "0x5233816151d616581993fca4988eea77d01e933d",
+    "0x7011f3edc7fa43c81440f9f43a6458174113b162",
+    "0x29bef3878b85522ef2afa541dd7477c9e0a2c122",
+    "0x2b22b2c65b185182786f087c1a0035b5ed76bbb7",
+    "0x83fccc659eeeee98ca9764b7b34409347dfbc98b",
+    "0xb328f05fa01cc2f9add1ed05c5636e7d9c9b9954",
+    "0x064e2f3511f2399e86953de1acf54322bbdf9d2d",
+];
---
+++ index.js
@@ -0,0 +1,29 @@
+const Crawler = require("crawler");
+const fs = require("fs");
+
+const writeFileCallback = (err, _) => console.log(err);
+const getData = ({ $ }) => ({
+    code: $("#editor").text(),
+    bytecode: $("#verifiedbytecode2").text(),
+    abi: $("#js-copytextarea2").text(),
+});
+const crawler = new Crawler({
+    maxConnections: 10,
+    callback: function (error, res, done) {
+        const address = res.options.uri.match(/address\/(.*?)#code/)[1];
+        const fName = `data/${address}`;
+        try {
+            const data = getData(res);
+            fs.writeFile(`${fName}.sol`, data.code, writeFileCallback);
+            fs.writeFile(`${fName}.bytecode`, data.bytecode, writeFileCallback);
+            fs.writeFile(`${fName}.abi`, data.abi, writeFileCallback);
+        } catch (err) {
+            console.log("crawler errro", error, err);
+        }
+        done();
+    },
+});
+const { addresses, blockExplorer } = require("./config");
+// console.log(addresses);
+
+addresses.slice(0, 3).forEach((e) => crawler.queue(blockExplorer(e)));
