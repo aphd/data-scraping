2ce2cdeb0af74bcd8ab22f28cbebf77fbd254b28
Antonio Pierro <antonio.pierro@gmail.com>
2021-01-09 15:34:24 +0100

    master

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const Crawler = require("crawler");
 const fs = require("fs");
+const { addresses, blockExplorer } = require("./config");
 
 const writeFileCallback = (err, _) => console.log(err);
+
 const getData = ({ $ }) => ({
     code: $("#editor").text(),
     bytecode: $("#verifiedbytecode2").text(),
     abi: $("#js-copytextarea2").text(),
 });
+
+const crawlerCallback = (error, res, done) => {
+    const address = res.options.uri.match(/address\/(.*?)#code/)[1];
+    const fName = `data/${address}`;
+    try {
+        const data = getData(res);
+        fs.writeFile(`${fName}.sol`, data.code, writeFileCallback);
+        fs.writeFile(`${fName}.bytecode`, data.bytecode, writeFileCallback);
+        fs.writeFile(`${fName}.abi`, data.abi, writeFileCallback);
+    } catch (err) {
+        console.log("Crawler error", error, err);
+    }
+    done();
+};
+
 const crawler = new Crawler({
     maxConnections: 10,
-    callback: function (error, res, done) {
-        const address = res.options.uri.match(/address\/(.*?)#code/)[1];
-        const fName = `data/${address}`;
-        try {
-            const data = getData(res);
-            fs.writeFile(`${fName}.sol`, data.code, writeFileCallback);
-            fs.writeFile(`${fName}.bytecode`, data.bytecode, writeFileCallback);
-            fs.writeFile(`${fName}.abi`, data.abi, writeFileCallback);
-        } catch (err) {
-            console.log("crawler errro", error, err);
-        }
-        done();
-    },
+    callback: crawlerCallback,
 });
-const { addresses, blockExplorer } = require("./config");
-// console.log(addresses);
 
-addresses.slice(0, 3).forEach((e) => crawler.queue(blockExplorer(e)));
+addresses.forEach((e) => crawler.queue(blockExplorer(e)));
